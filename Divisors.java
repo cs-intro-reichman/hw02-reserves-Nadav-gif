fa681f34977a30f4e18cab927a1aacd5fb3112af
Nadav Meroz <154219703+Nadav-gif@users.noreply.github.com>
2024-01-26 09:59:39 +0200

    Update Divisors.java

--- Divisors.java
+++ Divisors.java
@@ -2,7 +2,12 @@
  *  Gets a command-line argument (int), and prints all the divisors of the given number.
  */
 public class Divisors {
-	public static void main (String[] args) {
-		//// Put your code here
-	}
+   public static void main(String[] var0) {
+      int var1 = Integer.parseInt(var0[0]);
+      for(int var2 = 1; var2 <= var1; ++var2) {
+         if (var1 % var2 == 0) {
+            System.out.println(var2);
+         }
+      }
+   }
 }
